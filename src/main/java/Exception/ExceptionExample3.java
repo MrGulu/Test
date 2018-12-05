75686b08b0705602fd769e9bdb873d4bc20a1bda
tangwenlong <twlmail@163.com>
2018-12-05 16:24:59 +0800

    。

--- src/main/java/Exception/ExceptionExample3.java
+++ src/main/java/Exception/ExceptionExample3.java
@@ -14,7 +14,6 @@ public class ExceptionExample3 {
 //        System.out.println(s.length());//此处发生异常，不会继续往下执行，异常冒泡抛到了main，被catch捕获
             System.out.println("Oringinally create a MyException and throw it out!");
             throw new MyException("functionB函数异常！@！");
-
         } catch (Exception e) {
             logger.info("捕获到异常！");
             throw new MyException(e.getLocalizedMessage());
