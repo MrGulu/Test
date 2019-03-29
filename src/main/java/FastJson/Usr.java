60cbeb2f5a68e184691ca1b6e7e00c8d10e00b1e
tangwenlong <twlmail@163.com>
2019-03-29 15:19:25 +0800

    map转javabean

---
+++ src/main/java/FastJson/Usr.java
@@ -0,0 +1,67 @@
+package FastJson;
+
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Usr implements Serializable {
+    private static final long serialVersionUID = 2997216291673274672L;
+    private String instuCde;
+
+    private String usrCde;
+
+    private String extInd;
+
+    private String runInd;
+
+    private String usrName;
+
+    private String usrIdTyp;
+
+    private String usrIdNo;
+
+    private String usrPassword;
+
+    private String usrTel;
+
+    private String usrEmail;
+
+    private String usrBch;
+
+    private String depCde;
+
+    private String usrSts;
+
+    private String restInd;
+
+    private String usrRepassInd;
+
+    private String passModiDt;
+
+    private String usrRmk;
+
+    private String lastChgDt;
+
+    private String lastChgUsr;
+
+    private String managInd;
+
+    private String usrSuper;
+
+    private String riskGrd;
+
+    private String quGrd;
+
+    private String specialRole;
+
+    private String isRate;
+
+    private String assistToSign;
+
+    private String empDt;
+
+    private String cooprCde;
+
+}
--- src/main/java/utils/JacksonUtil.java
+++ src/main/java/utils/JacksonUtil.java
@@ -5,6 +5,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -149,4 +155,47 @@ public class JacksonUtil {
 			}
 		 return  objectMapper.getTypeFactory().constructParametricType(collectionClass, elementClasses); 
 	 }
+
+
+	/**
+	 * @param type 实体类class对象类型
+	 * @param map  要转化的map
+	 * @return Object 强转成想要的实体类型
+	 */
+	public static Object convertMap(Class type, Map map)
+			throws IntrospectionException, IllegalAccessException,
+			InstantiationException, InvocationTargetException {
+		// 获取类属性
+		BeanInfo beanInfo = Introspector.getBeanInfo(type);
+		// 创建 JavaBean 对象
+		Object obj = type.newInstance();
+
+		// 给 JavaBean 对象的属性赋值
+		PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+		for (PropertyDescriptor descriptor : propertyDescriptors) {
+			//获取属性名
+			String propertyName = descriptor.getName();
+			//获取属性类型
+			String propertyType = descriptor.getPropertyType().getTypeName();
+			if (map.containsKey(propertyName)) {
+				System.out.println("你好，我是：" + propertyName + "! 我长这个样：" + map.get(propertyName));
+				// 下面一句可以 try 起来，这样当一个属性赋值失败的时候就不会影响其他属性赋值。
+				Object[] args = new Object[1];
+				//如果数据库类型是BigDecimal，传来不可以是非BigDecimal类型或者为空和null，会产生参数类型不匹配异常，
+				//这里将需要类型为BigDecimal的前台数据做类型转换
+				if ("java.math.BigDecimal".equals(propertyType)) {
+					if (map.get(propertyName) == null || "".equals(map.get(propertyName))) {
+						continue;
+					}
+					BigDecimal value = new BigDecimal(map.get(propertyName).toString());
+					args[0] = value;
+				} else {
+					Object value = map.get(propertyName);
+					args[0] = value != null ? value.toString() : "";
+				}
+				descriptor.getWriteMethod().invoke(obj, args);
+			}
+		}
+		return obj;
+	}
 }
