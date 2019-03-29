37507d9c7e0aca2a31bc04f36ba576c52a01fa59
tangwenlong <twlmail@163.com>
2019-03-29 16:01:11 +0800

    循环测试continue

--- src/main/java/forloop/Test.java
+++ src/main/java/forloop/Test.java
@@ -11,4 +11,21 @@ public class Test {
         }
         System.out.println(i);
     }
+
+    @org.junit.Test
+    public void test2() {
+        for (int w = 0; w < 5; w++) {
+            int i = 2, j = 2, k = 1;
+            if (i == 1) {
+                if (j == 1) {
+                    continue;
+                }
+                k = 2;
+                System.out.println(k);
+            } else {
+                k = 3;
+                System.out.println(k);
+            }
+        }
+    }
 }
