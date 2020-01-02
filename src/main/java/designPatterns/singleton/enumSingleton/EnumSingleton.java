b34ac038a2e987a3b200f6f9971e9c2efdef1d11
tangwenlong <twlmail@163.com>
2020-01-02 11:49:51 +0800

    枚举单例模式优化

--- src/main/java/designPatterns/singleton/enumSingleton/EnumSingleton.java
+++ src/main/java/designPatterns/singleton/enumSingleton/EnumSingleton.java
@@ -23,6 +23,14 @@ public enum EnumSingleton {
         User user = EnumSingleton.INSTANCE.getInstance();
         user.setName("tang").setAge(18).setAddress("China");
         System.out.println(user);
+        //获取的还是同一个User对象，对其修改，都是修改同一个对象
+        User user2 = EnumSingleton.INSTANCE.getInstance();
+        user2.setName("yu").setAge(18).setAddress("China");
+        System.out.println(user);
+        System.out.println(user2);
+        //true，说明同一个对象
+        System.out.println(user == user2);
+
     }
 }
 
--- src/main/java/designPatterns/singleton/enumSingleton/EnumTest.java
+++
@@ -1,8 +0,0 @@
-package designPatterns.singleton.enumSingleton;
-
-public class EnumTest {
-    public static void main(String[] args) {
-        User user = EnumSingleton.INSTANCE.getInstance();
-        System.out.println(user);
-    }
-}
