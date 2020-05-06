a9355cd7e473b1bee3a7bccf8f039511e46eb1d5
tangwenlong <twlmail@163.com>
2020-05-06 15:59:07 +0800

    guava简测。

---
+++ src/main/java/guava/Test.java
@@ -0,0 +1,12 @@
+package guava;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Map<String, Object> map = Maps.newHashMap();
+    }
+}
